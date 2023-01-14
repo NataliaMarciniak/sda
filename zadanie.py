06b9f72d74238afde5bc17941fa71b7bb43f330d
Natalia <marciniak.natalia95@gmail.com>
2023-01-14 11:39:47 +0100

    zadanie wykonane 3

--- zadanie.py
+++ zadanie.py
@@ -1,32 +1,13 @@
-
-def sort_list(numbers):
-    passed_list = []
-    for element in numbers:
-        if isinstance(element, int):
-            passed_list.append(element)
+example = [1, [2, 3, [4, 5]], [[[7]]], 8, [[[[9]]], 10]]
+flat_list = []
+def flatten_list(old_list):
+    for element in old_list:
+        if type(element) == list:
+            flatten_list(element)
         else:
-            if isinstance(element, str):
-                continue
-            elif isinstance(element, list):
-                for x in element:
-                    if isinstance(x, int):
-                        passed_list.append(x)
-                    elif isinstance(x, str):
-                        continue
-                    elif isinstance(x, list):
-                        for y in x:
-                            if isinstance(y, int):
-                                passed_list.append(y)
-                            elif isinstance(y, str):
-                                continue
-                            elif isinstance(y, list):
-                                for z in y:
-                                    if isinstance(z, int):
-                                        passed_list.append(z)
-                                    elif isinstance(z, str):
-                                        continue
-    return passed_list
+            flat_list.append(element)
 
-example = [1, [2, 3, [4, 5]], 7, 8, [[9], [[[10]]]]]
+
+flatten_list(example)
 print("Original list: ", example)
-print("New list: ", sort_list(example))
+print("New list: ", flat_list)
